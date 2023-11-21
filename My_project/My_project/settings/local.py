58f1ac3f0cbda3ea28d84b42ce3a6e91cf86bb0b
Leonardofffajardo <wilmarleofa@hotmail.com>
2023-11-21 20:51:12 +0000

    BBDD_1

--- My_project/My_project/settings/local.py
+++ My_project/My_project/settings/local.py
@@ -3,19 +3,19 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leonardofff.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbempleado',
-        'USER': 'leonardo',
-        'PASSWORD': 'Valentina2024',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leonardofff$default',
+        'USER': 'leonardofff',
+        'PASSWORD': 'Arroz1516',
+        'HOST': 'leonardofff.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
